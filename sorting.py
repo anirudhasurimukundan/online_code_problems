586895dd0d6e636fd95a03769fe93f1171bf2e77
amukundan <anirudh.asurimukundan@gmail.com>
2023-06-14 22:47:37 -0700

    Added quick sort algorithm

--- sorting.py
+++ sorting.py
@@ -1,6 +1,32 @@
 import numpy as np
 
 class Sort:
+    def quickSort(self, arr):
+        """
+        Take the last element of array as pivot
+        Pop that element from the array
+        Assign the remainder chunk of the array into two sub arrays - array1: elements greater than pivot and array2: elements lesser than pivot
+        Return the sorted array using recursion
+        """
+        length = len(arr)
+        
+        # sanity check to return single element array
+        if length <= 1:
+            return arr
+        else:
+            pivot = arr.pop()
+
+        items_lesser = []
+        items_greater = []
+
+        for i in arr:
+            if i >= pivot:
+                items_greater.append(i)
+            else:
+                items_lesser.append(i)
+        return self.quickSort(self, items_lesser) + [pivot] + self.quickSort(self, items_greater)
+
+
     def insertionSort(self, arr):
         """
         Find the smallest element of the remaining chunk of the array
@@ -15,6 +41,7 @@ class Sort:
 
 if __name__ == "__main__":
     sol = Sort;
-    #arr = [1, 4, 2, 8, 0]
-    arr = np.random.randint(0, 10, size=(1000), dtype=int)
-    print("Sorted array = ", sol.insertionSort(sol, arr))
+    arr = [1, 4, 2, 8, 0, 3, 4, 4]
+    #arr = np.random.randint(0, 10, size=(1000), dtype=int)
+    #print("Sorted array using insertion sort = ", sol.insertionSort(sol, arr))
+    print("Sorted array using quick sort = ", sol.quickSort(sol, arr))
