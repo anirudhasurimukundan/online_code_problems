6f17fcfcbfe203f4549b381c662160b7d8ccff32
amukundan <anirudh.asurimukundan@gmail.com>
2023-06-14 19:57:15 -0700

    Updated binary search with <= instead of < in while loop condition

--- search.py
+++ search.py
@@ -4,27 +4,37 @@ for i in range(n):
     x[i], x[min_index] = x[min_index], x[i]
 """
 class Search:
+    def linearSearch(self, arr, lookup):
+        """
+        This search algorithm searches through the entire array to look for the lookup number
+        Time complexity: O(n)
+        Space complexity: O(n)
+        """
+        for i in arr:
+            if i == lookup:
+                return(f"{lookup} found at location {i} in {arr}")
+        return (f"{lookup} not found in {arr}")
+
     def binarySearch(self, arr, lookup):
         """
         This search algorithm divides and conquers the search process.
         Time complexity: O(logn)
-        Space complexity: O(n) with n being number of elements in array "arr"
+        Space complexity: O(logn) with n being number of elements in array "arr"
         """
         lbound = 0
         rbound = len(arr)-1
-        i = 0
-        while lbound < rbound:
+        while lbound <= rbound:
             mid = int((lbound + rbound)*0.5)
             if lookup == arr[mid]:
-                return("Found at location ", ind)
+                return(f"{lookup} found at location {mid} in {arr}")
             elif lookup < arr[mid]: # if lookup value is less than mid
                 rbound = mid-1
-            elif lookup > arr[mid]:
+            elif lookup > arr[mid]: # if lookup value is greater than mid
                 lbound = mid+1
         return (f"{lookup} not found in {arr}")
 
 if __name__ == "__main__":
     arr = [1, 2, 4, 6, 9, 10]
-    lookup = 7
+    lookup = 6
     sol = Search;
     print(sol.binarySearch(sol, arr, lookup))
