481bf300b1d0a4c15dac13ee736ccf8b64e1c1b8
amukundan <anirudh.asurimukundan@gmail.com>
2023-06-15 11:18:10 -0700

    Added binary search using recursion

--- search.py
+++ search.py
@@ -30,11 +30,31 @@ class Search:
                 lbound = mid+1
         return (f"{lookup} not found in {arr}")
 
+    def binarySearch_with_recursion(self, arr, lookup):
+        """
+        Use recursion to do binary search
+        """
+        if(len(arr) == 1):
+            return (f"Sorted array = {arr}")
+
+        lbound = 0
+        rbound = len(arr) - 1
+        while lbound <= rbound:
+            mid = (lbound + rbound) // 2
+            if lookup == arr[mid]:
+                return(f"{lookup} is found at location {mid} in {arr}")
+            elif lookup < arr[mid]:
+                return (sol.binarySearch_with_recursion(sol, arr[:mid], lookup))
+            elif lookup > arr[mid]:
+                return(sol.binarySearch_with_recursion(sol, arr[mid:], lookup))
+
+
 if __name__ == "__main__":
-    #arr = [1, 2, 4, 6, 9, 10]
+    arr = [1, 2, 4, 6, 9, 10]
     n = 1000
-    arr = np.random.randint(0, 10, size=(1000), dtype=int)
+    #arr = np.random.randint(0, 10, size=(1000), dtype=int)
     lookup = 6
     sol = Search;
     print("Binary search: ", sol.binarySearch(sol, arr, lookup))
     print("Linear search: ", sol.linearSearch(sol, arr, lookup))
+    print("Binary search with recursion: ", sol.binarySearch_with_recursion(sol, arr, lookup))
