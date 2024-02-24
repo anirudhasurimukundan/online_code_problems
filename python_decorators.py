15df894867738f9d7d3e52d58ea97263bcdd8f65
amukundan <anirudh.asurimukundan@gmail.com>
2024-02-23 23:49:14 -0500

    Added lru_cache in python decorators exploration to store max of 5 previous values

--- python_decorators.py
+++ python_decorators.py
@@ -1,6 +1,7 @@
-from functools import cache
+from functools import cache, lru_cache
 
-@cache
+# @cache # stores all the previous values
+@lru_cache(maxsize=5) # storing last 5 values 
 def fib(n):
     return (n if n <= 1 else fib(n-1) + fib(n-2))
 
