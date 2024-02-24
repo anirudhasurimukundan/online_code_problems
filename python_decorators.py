7fc39b1c09054bd37832868a908d55e0844fdcb1
amukundan <anirudh.asurimukundan@gmail.com>
2024-02-23 23:46:45 -0500

    Added python decorator python files for exploration

---
+++ python_decorators.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+@cache
+def fib(n):
+    return (n if n <= 1 else fib(n-1) + fib(n-2))
+
+def main():
+    for i in range(400):
+        print(i, fib(i))
+    print("done")
+
+if __name__ == "__main__":
+    main()
